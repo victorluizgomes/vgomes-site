e339f859e728baa62c6a8788b951bcd3be151ba1
Victor Gomes <gomesvictorluiz@gmail.com>
2022-11-05 17:59:09 -0400

    ✅ add footer styles and links

--- apps/vgomes-site/src/app/components/footer/footer.tsx
+++ apps/vgomes-site/src/app/components/footer/footer.tsx
@@ -1,3 +1,9 @@
+import {
+  TWITTER_URL,
+  INSTAGRAM_URL,
+  GITHUB_URL,
+  LINKEDIN_URL,
+} from '../../../model/constants';
 import styles from './footer.module.scss';
 
 /* eslint-disable-next-line */
@@ -5,8 +11,36 @@ export interface FooterProps {}
 
 export function Footer(props: FooterProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Footer!</h1>
+    <div className={`${styles['container']} py-20`}>
+      <div className="px-16">
+        <p>
+          Say Hello &#8212;{' '}
+          <a
+            className="hover:border-b-gold-yellow hover:border-b-2"
+            href="mailto:vgomescontact@gmail.com"
+          >
+            vgomescontact@gmail.com
+          </a>
+        </p>
+        <hr className={`my-6 border-gold-yellow border-t-4`} />
+        <div className="flex justify-between">
+          <p>&#169; Victor Gomes {new Date().getFullYear()}</p>
+          <ul className="flex gap-5">
+            <li className="hover:border-b-gold-yellow hover:border-b-2">
+              <a href={TWITTER_URL}>TW</a>
+            </li>
+            <li className="hover:border-b-gold-yellow hover:border-b-2">
+              <a href={INSTAGRAM_URL}>IG</a>
+            </li>
+            <li className="hover:border-b-gold-yellow hover:border-b-2">
+              <a href={GITHUB_URL}>GH</a>
+            </li>
+            <li className="hover:border-b-gold-yellow hover:border-b-2">
+              <a href={LINKEDIN_URL}>LN</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ apps/vgomes-site/src/model/constants.tsx
@@ -0,0 +1,4 @@
+export const TWITTER_URL = 'https://twitter.com/vgomes_eth';
+export const INSTAGRAM_URL = 'https://instagram.com/vgomes.arts';
+export const GITHUB_URL = 'https://github.com/victorluizgomes';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/victorluizgomes/';
