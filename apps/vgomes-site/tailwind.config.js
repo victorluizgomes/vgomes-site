87ce6cc314d56699a7e0bf51fe6952aa6fd6366c
Victor Gomes <gomesvictorluiz@gmail.com>
2022-11-05 02:17:54 -0400

    add tailwind color palette

--- apps/vgomes-site/tailwind.config.js
+++ apps/vgomes-site/tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'off-white': '#FCFCF8',
+      'off-grey': '#E5E5E5',
+      'smoky-black': '#1D0F0C',
+      'gold-yellow': '#E8BA74',
+      'mid-blue': '#88C4D3',
+      'mid-red': '#E38671'
+    }
   },
   plugins: [],
 };
